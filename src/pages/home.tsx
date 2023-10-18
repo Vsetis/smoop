4b1635463aa46b72138e47944d8cc1d3157edd6d
Vsetis <126694324+Vsetis@users.noreply.github.com>
2023-10-18 15:34:50 +0200

    fix homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,11 +2,12 @@ import CreatePost from '@/components/Post/CreatePost';
 import PostCard from '@/components/Post/PostCard';
 import MainSection from '@/components/User/MainSection';
 import UserProfile from '@/components/UserProfile';
-import { posts } from '@/mock/posts';
-import { useUser } from '@/utils/atom';
+
+import { usePosts, useUser } from '@/utils/atom';
 
 export default function Home() {
     const [user, setUser] = useUser();
+    const [posts, setPosts] = usePosts();
 
     const postQuery = posts.filter((p) => p.userId !== user!.id);
 
@@ -17,19 +18,22 @@ export default function Home() {
             </div>
             <MainSection title="Home">
                 <div className="flex flex-col">
-                    {postQuery.map((post) => (
-                        <PostCard
-                            key={post.id}
-                            id={post.id}
-                            userId={post.userId}
-                            content={post.content}
-                            isLiked={post.liked}
-                            count={{
-                                likes: post.likes,
-                                comments: post.comments?.length || 0,
-                            }}
-                        />
-                    ))}
+                    {postQuery
+                        .map((post) => (
+                            <PostCard
+                                key={post.id}
+                                id={post.id}
+                                userId={post.userId}
+                                content={post.content}
+                                isLiked={post.liked}
+                                postRouter={true}
+                                count={{
+                                    likes: post.likes,
+                                    comments: post.comments?.length || 0,
+                                }}
+                            />
+                        ))
+                        .reverse()}
                 </div>
             </MainSection>
         </UserProfile>
