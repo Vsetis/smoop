52cd658a869f4d364d88b6e459e2bd2738b4c61b
Vsetis <126694324+Vsetis@users.noreply.github.com>
2023-10-18 19:44:07 +0200

    create homepage trends

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,11 +5,49 @@ import UserProfile from '@/components/UserProfile';
 
 import { usePosts, useUser } from '@/utils/atom';
 
+import { useReducer } from 'react';
+
+type State = {
+    postPerPage: number;
+    error: string | null;
+};
+
+type Action = {
+    type: 'load';
+};
+
+function reducer(state: State, action: Action) {
+    const { type } = action;
+
+    switch (type) {
+        case 'load': {
+            const newLoad = state.postPerPage + 5;
+            const hasError = newLoad > 50;
+            return {
+                ...state,
+                postPerPage: hasError ? state.postPerPage : newLoad,
+                error: hasError ? 'There is nothing new!' : null,
+            };
+        }
+        default:
+            return state;
+    }
+}
+
 export default function Home() {
     const [user, setUser] = useUser();
-    const [posts, setPosts] = usePosts();
+    const [post, setPost] = usePosts();
 
-    const postQuery = posts.filter((p) => p.userId !== user!.id);
+    const [state, dispatch] = useReducer(reducer, {
+        postPerPage: 5,
+        error: null,
+    });
+
+    const postQuery = post.filter((p) => p.userId !== user!.id);
+
+    const sortedPost = [...postQuery]
+        .sort((a, b) => b.likes - a.likes)
+        .slice(0, state.postPerPage);
 
     return (
         <UserProfile user={user!}>
@@ -17,23 +55,31 @@ export default function Home() {
                 <CreatePost avatar={user!.avatar} username={user!.username} />
             </div>
             <MainSection title="Home">
-                <div className="flex flex-col">
-                    {postQuery
-                        .map((post) => (
-                            <PostCard
-                                key={post.id}
-                                id={post.id}
-                                userId={post.userId}
-                                content={post.content}
-                                isLiked={post.liked}
-                                postRouter={true}
-                                count={{
-                                    likes: post.likes,
-                                    comments: post.comments?.length || 0,
-                                }}
-                            />
-                        ))
-                        .reverse()}
+                <div>
+                    {sortedPost.map((post) => (
+                        <PostCard
+                            key={post.id}
+                            id={post.id}
+                            userId={post.userId}
+                            content={post.content}
+                            isLiked={post.liked}
+                            postRouter={true}
+                            count={{
+                                likes: post.likes,
+                                comments: post.comments?.length || 0,
+                            }}
+                        />
+                    ))}
+                    {state.error ? (
+                        <p>There is nothing new!</p>
+                    ) : (
+                        <button
+                            className="w-full font-semibold py-2 transition-all hover:bg-white/5"
+                            onClick={() => dispatch({ type: 'load' })}
+                        >
+                            More...
+                        </button>
+                    )}
                 </div>
             </MainSection>
         </UserProfile>
