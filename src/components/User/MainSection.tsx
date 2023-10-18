5ed2c58f5d23ce6fe4e76a6783e1f165aac4e046
Vsetis <126694324+Vsetis@users.noreply.github.com>
2023-10-18 19:42:23 +0200

    create homepage trends

--- src/components/User/MainSection.tsx
+++ src/components/User/MainSection.tsx
@@ -6,11 +6,13 @@ export default function MainSection({
     children: React.ReactNode;
 }) {
     return (
-        <div className="rounded md:border border-white/20 w-full min-h-[calc(100vh-350px)]">
-            <div className="p-2 md:p-4 border-white/20">
-                <h2 className="font-semibold text-white/80">{title}</h2>
+        <div>
+            <div className="rounded md:border border-white/20 w-full h-full">
+                <div className="p-2 md:p-4 border-white/20">
+                    <h2 className="font-semibold text-white/80">{title}</h2>
+                </div>
+                {children}
             </div>
-            {children}
         </div>
     );
 }
